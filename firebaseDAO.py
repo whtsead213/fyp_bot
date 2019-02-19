e352f8fbdda2ee49275bc50ca9a2eef29cf669ff
alberthsies <alberthsies@gmail.com>
2019-02-20 01:21:51 +0800

    Implement DAO functions

--- firebaseDAO.py
+++ firebaseDAO.py
@@ -4,6 +4,61 @@ from firebase import firebase
 
 #firebase = firebase.FirebaseApplication('https://ml-sec-fyp.firebaseio.com', None)
 firebase = firebase.FirebaseApplication('https://hkust-fyp-ricwta01.firebaseio.com/', None)
+
+
+def append_accounts():
+    if firebase.get('/accounts', None):
+        return [key for key in firebase.get('/accounts', None).keys()]
+    return []
+
+
+def firebase_get(item=None, key=None):
+    if item == None:
+        return None
+    
+    return firebase.get(item, key)
+
+
+def firebase_put(item=None, key=None, value=None):
+    if item == None or key == None:
+        return
+    
+    firebase.put(item, key, value)
+    return
+
+
+def get_account(email):
+    return firebase.get('/accounts', email)
+
+
+def set_account(email, account):
+    firebase.put('/accounts', email, account)
+    return
+
+
+def delete_account(account_name=None):
+    firebase.delete('/accounts', account_name)
+    return
+
+
+def get_order(email):
+    return firebase.get('/accounts/' + email, 'orders')
+
+
+def set_order(email, order):
+    firebase.put('/accounts/' + email, 'orders', order)
+    return
+
+
+def get_passwd(email):
+    return firebase.get('/accounts/' + email, 'pw')
+
+
+def set_passwd(email, password):
+    firebase.put('/accounts/' + email, 'pw', password)
+    return
+
+
 def attack_record(attack_type=None, attack_scenario=None, attack_time=None, attacker=None):
     if attack_type == None or attack_scenario == None:
         return
