cce1cd10ae247d686e05a88155b38c4d4b54b856
alberthsies <alberthsies@gmail.com>
2019-02-22 10:08:16 +0800

    Implement tunneling method

--- bot.py
+++ bot.py
@@ -4,13 +4,28 @@ import random
 from datetime import datetime
 from selenium.common.exceptions import NoSuchElementException
 from scenario import scenario_list, attack_scenario_list
+from sshtunnel import SSHTunnelForwarder
 from selenium import webdriver
 import config
 import firebaseDAO
 
-def create_driver(url):
+server = None
+
+def create_driver(url,port):
+    global server
+    server = SSHTunnelForwarder(
+        ('vml1wk054.cse.ust.hk', 22),
+        ssh_username="bot",
+        ssh_password="tobtobtobtob",
+        remote_bind_address=('127.0.0.1', port),
+        local_bind_address=('127.0.0.1', port)
+    )
+    server.start()
+    print(server.local_bind_port)  # show assigned local port
+    # work with `SECRET SERVICE` through `server.local_bind_port`.
+    
     driver = webdriver.Chrome('./chromedrivers/chromedriver')
-    driver.get(config.config[url])
+    driver.get('localhost:' + str(port))
 
     try:
         #dismiss the cookie message since it make the close button untouchable
@@ -20,6 +35,7 @@ def create_driver(url):
         print('can not find cookie message')
         pass
     
+    #server.stop()
     return driver
 
 if __name__ == '__main__':
@@ -39,7 +55,7 @@ if __name__ == '__main__':
     starting_time = str(datetime.now())
 
     if mode == 'random' or mode == 'r':
-        driver = create_driver(url="url")
+        driver = create_driver(url="url", port=config.config['normal_port'])
         for i in range(int(sys.argv[2])):
             try:
                 scenario = random.choice(scenario_list)
@@ -49,7 +65,7 @@ if __name__ == '__main__':
             except NoSuchElementException:
                 continue
     if mode == 'custom' or mode == 'c':
-        driver = create_driver(url="url")
+        driver = create_driver(url="url", port=config.config['normal_port'])
         for i in range(len(sys.argv)):
             if i == 0 or i == 1:
                 continue
@@ -76,15 +92,15 @@ if __name__ == '__main__':
                     continue
                 else:
                     if index == 0 or index == 1:
-                        attack_url = "xss_url"
+                        attack_port = "xss_port"
                         attack_type = "xss"
-                    elif index == 2:
-                        attack_url = "sql_url"
+                    elif index == 2 or index == 3:
+                        attack_port = "sql_potr"
                         attack_type = "sql_inj"
                     else:
-                        attack_url = "normal_url"
+                        attack_port = "normal_potr"
                         attack_type = "other"
-                    driver = create_driver(url=attack_url)
+                    driver = create_driver(url="url", port=config.config[attack_port])
                     try:
                         firebaseDAO.attack_record(attack_type=attack_type, attack_scenario=str(attack_scenario_list[index]), attack_time=str(datetime.now()), attacker="PLEASE TYPE YOUR NAME HERE")
                         attack_scenario_list[index](driver)
